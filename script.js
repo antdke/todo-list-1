9b5f1c193801ae789fa232a8023a6370a74bc896
Anthony Diké <anthonydike@nyu.edu>
2022-02-21 23:24:45 +0000

    can create tasks

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ function $(id){
 
 let i = $("input")
 let t = $("todos")
+let tb = $("textbox")
+let taskText = "";
 
+tb.addEventListener("change", createTaskText)
 i.addEventListener("submit", addTodo)
+
 
 // TODOS
 function addTodo(e) {
   e.preventDefault()
   let todo = document.createElement("div")
-  todo.append()
+  let task = document.createElement("p")
+  task.innerText = taskText
+  let doneButton = document.createElement("button")
+  doneButton.innerText = "Done!"
+  todo.append(task, doneButton)
   t.append(todo)
 }
 
-
+function createTaskText(e) {
+  e.preventDefault()
+  taskText = tb.value
+}
